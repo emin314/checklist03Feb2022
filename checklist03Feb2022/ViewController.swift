9072b2c9aa4434c43dde3ad6374a7b1d2568bb3f
emin <50670112+emin314@users.noreply.github.com>
2022-02-03 22:50:41 +0300

    Initial Commit

---
+++ checklist03Feb2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  checklist03Feb2022
+//
+//  Created by Emin on 3.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
